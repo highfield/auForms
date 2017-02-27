72c37e8a4d5eba325ad7edac59102816850e1852
vernarim <mario.vernari@cet-electronics.com>
2017-02-27 08:58:31 +0100

    added a new wpf-edition converter

---
+++ auFormsConverterWPF/MainWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Cet.Aurora.Web.PageBuilder;
+using Cet.UI;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace auFormsConverterWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.BtnSelectFolder.Click += BtnSelectFolder_Click;
+            this.BtnRefresh.Click += BtnRefresh_Click;
+            this.BtnReload.Click += BtnReload_Click;
+            this.BtnCopy.Click += BtnCopy_Click;
+            this.LstFiles.ItemsSource = this._files;
+            this.LstFiles.SelectionChanged += LstFiles_SelectionChanged;
+        }
+
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.TxtPath.Text = Properties.Settings.Default.SelectedPath;
+            this.ScanFolder();
+            this.OpenSourceFile(null);
+        }
+
+
+        private ObservableCollection<string> _files = new ObservableCollection<string>();
+        private string _currentSourceFile;
+        private string _minifiedOutput;
+
+
+        private void BtnSelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            string[] folders = CommonDialogs.ChooseFolder(
+                this,
+                this.TxtPath.Text,
+                Environment.SpecialFolder.Desktop,
+                showNewFolderButton: true,
+                title: "Select source files folder"
+                );
+
+            if (folders.Any())
+            {
+                this.TxtPath.Text = folders[0];
+                this.ScanFolder();
+            }
+        }
+
+
+        private void BtnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            this.ScanFolder();
+        }
+
+
+        private void BtnReload_Click(object sender, RoutedEventArgs e)
+        {
+            this.OpenSourceFile(this._currentSourceFile);
+        }
+
+
+        private void BtnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this._minifiedOutput) == false)
+            {
+                Clipboard.SetText(this._minifiedOutput);
+            }
+        }
+
+
+        private void ScanFolder()
+        {
+            Properties.Settings.Default.SelectedPath = this.TxtPath.Text;
+            Properties.Settings.Default.Save();
+
+            this._files.Clear();
+            this._currentSourceFile = null;
+            this._minifiedOutput = null;
+            try
+            {
+                var path = this.TxtPath.Text ?? string.Empty;
+                var folder = new System.IO.DirectoryInfo(path);
+                foreach (System.IO.FileInfo fi in folder.EnumerateFiles("*.xml"))
+                {
+                    this._files.Add(fi.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    this,
+                    ex.Message,
+                    "Error!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+            }
+        }
+
+        private void LstFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var fname = this.LstFiles.SelectedItem as string;
+            this._currentSourceFile = string.IsNullOrEmpty(fname)
+                ? string.Empty
+                : System.IO.Path.Combine(this.TxtPath.Text, fname);
+            this.OpenSourceFile(this._currentSourceFile);
+        }
+
+
+        private void OpenSourceFile(string fileIn)
+        {
+            this._minifiedOutput = null;
+            this.TxtSource.Text = string.Empty;
+            this.TxtTarget.Text = string.Empty;
+            this.TxtSource.Background = this.Background;
+            this.TxtTarget.Background = this.Background;
+            if (string.IsNullOrEmpty(fileIn)) return;
+
+            XDocument xin;
+            try
+            {
+                xin = XDocument.Load(fileIn);
+            }
+            catch (Exception ex)
+            {
+                this.TxtSource.Text = ex.Message;
+                this.TxtSource.Background = Brushes.LightPink;
+                return;
+            }
+
+            if (xin.Root?.Name.LocalName != "auForm")
+            {
+                this.TxtSource.Text = "Not a valid 'auForm' template.";
+                this.TxtSource.Background = Brushes.LightPink;
+                return;
+            }
+
+            this.TxtSource.Text = xin.ToString();
+            this.TxtSource.Background = Brushes.PaleGreen;
+
+            JObject jout;
+            try
+            {
+                jout = AuFormsHelpers.ConvertRoot(xin);
+            }
+            catch (Exception ex)
+            {
+                this.TxtTarget.Text = ex.Message;
+                this.TxtTarget.Background = Brushes.LightPink;
+                return;
+            }
+
+            try
+            {
+                string fileOut = System.IO.Path.ChangeExtension(fileIn, ".json");
+                using (System.IO.StreamWriter file = System.IO.File.CreateText(fileOut))
+                using (var writer = new Newtonsoft.Json.JsonTextWriter(file))
+                {
+                    jout.WriteTo(writer);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.TxtTarget.Text = ex.Message;
+                this.TxtTarget.Background = Brushes.LightPink;
+            }
+
+            var sb = new StringBuilder();
+            using (var sw = new System.IO.StringWriter(sb))
+            using (var writer = new Newtonsoft.Json.JsonTextWriter(sw))
+            {
+                jout.WriteTo(writer);
+            }
+            this._minifiedOutput = sb.ToString();
+
+            this.TxtTarget.Text = jout.ToString(Newtonsoft.Json.Formatting.Indented);
+
+            this.TxtTarget.Background = Brushes.PaleGreen;
+        }
+
+    }
+}
---
+++ auFormsConverterWPF/Utils/CommonDialogs.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+//using System.Windows.Forms;
+using System.Windows.Interop;
+using Microsoft.Win32;
+
+
+namespace Cet.UI
+{
+    public static class CommonDialogs
+    {
+        public const string DefaultTextFilter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+        public const string DefaultXmlFilter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+        public const string DefaultImageFilter = "Image files (*.bmp, *.gif, *.ico, *.jpg, *.png, *.wdp, *.tiff)|*.bmp;*.gif;*.ico;*.jpg;*.png;*.wdp;*.tiff|All files (*.*)|*.*";
+
+        public const string DefaultImageTitle = "Select image...";
+
+
+        public static string[] OpenFile(
+            DependencyObject owner,
+            string initialFileName,
+            bool multipleSelection,
+            string title,
+            string filter
+            )
+        {
+            var dialog = new OpenFileDialog();
+            dialog.AddExtension = false;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            dialog.DereferenceLinks = false;
+            dialog.FileName = initialFileName;
+            dialog.Filter = filter;
+            dialog.Multiselect = multipleSelection;
+            dialog.RestoreDirectory = true;
+            dialog.Title = title;
+
+            bool? result;
+
+            if (owner == null)
+            {
+                result = dialog.ShowDialog();
+            }
+            else
+            {
+                result = dialog.ShowDialog(Window.GetWindow(owner));
+            }
+
+            if (result == true)
+            {
+                //conferma la scelta
+                return dialog.FileNames;
+            }
+
+            return new string[0];
+        }
+
+
+        public static string[] OpenImageFile(
+            DependencyObject owner,
+            string initialFileName,
+            bool multipleSelection,
+            string title = DefaultImageTitle,
+            string filter = DefaultImageFilter
+            )
+        {
+            var dialog = new OpenFileDialog();
+            dialog.AddExtension = false;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            dialog.DereferenceLinks = false;
+            dialog.FileName = initialFileName;
+            dialog.Filter = filter;
+            dialog.Multiselect = multipleSelection;
+            dialog.RestoreDirectory = true;
+            dialog.Title = title;
+
+            bool? result;
+
+            if (owner == null)
+            {
+                result = dialog.ShowDialog();
+            }
+            else
+            {
+                result = dialog.ShowDialog(Window.GetWindow(owner));
+            }
+
+            if (result == true)
+            {
+                //conferma la scelta
+                return dialog.FileNames;
+            }
+
+            return new string[0];
+        }
+
+
+        public static string[] SaveFile(
+            DependencyObject owner,
+            string initialFileName,
+            bool overwritePrompt,
+            string title,
+            string filter
+            )
+        {
+            var dialog = new SaveFileDialog();
+            dialog.AddExtension = true;
+            dialog.CheckFileExists = false;
+            dialog.CheckPathExists = true;
+            dialog.DereferenceLinks = false;
+            dialog.FileName = initialFileName;
+            dialog.Filter = filter;
+            dialog.OverwritePrompt = overwritePrompt;
+            dialog.RestoreDirectory = true;
+            dialog.Title = title;
+
+            bool? result;
+
+            if (owner == null)
+            {
+                result = dialog.ShowDialog();
+            }
+            else
+            {
+                result = dialog.ShowDialog(Window.GetWindow(owner));
+            }
+
+            if (result == true)
+            {
+                //conferma la scelta
+                return dialog.FileNames;
+            }
+
+            return new string[0];
+        }
+
+
+#if true
+        public static string[] ChooseFolder(
+            DependencyObject owner,
+            string selectedPath,
+            Environment.SpecialFolder rootFolder,
+            bool showNewFolderButton,
+            string title
+            )
+        {
+            var collection = new List<string>();
+
+            using (var fsd = new FolderSelectDialog())
+            {
+                fsd.Title = title;
+                fsd.InitialDirectory = selectedPath;
+
+                var window = Window.GetWindow(owner);
+                var wih = new WindowInteropHelper(window);
+
+                if (fsd.ShowDialog(wih.Handle))
+                {
+                    collection.Add(fsd.FileName);
+                }
+            }
+
+            return collection.ToArray();
+        }
+#else
+        public static string[] ChooseFolder(
+            DependencyObject owner,
+            string selectedPath,
+            Environment.SpecialFolder rootFolder,
+            bool showNewFolderButton,
+            string description)
+        {
+            //crea l'istanza di dialog
+            using (var browser = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                //fissa i parametri di visualizzazione
+                browser.ShowNewFolderButton = showNewFolderButton;
+                browser.RootFolder = rootFolder;
+                browser.SelectedPath = selectedPath;
+                browser.Description = description;
+
+                //mostra il dialog ed attende il risultato
+                System.Windows.Forms.DialogResult result;
+
+                if (owner == null)
+                {
+                    result = browser.ShowDialog();
+                }
+                else
+                {
+                    //e' necessaria una conversione tra Wpf e WinForms
+                    var window = Window.GetWindow(owner);
+                    var wih = new WindowInteropHelper(window);
+                    var win32 = new Win32Window
+                    {
+                        Handle = wih.Handle
+                    };
+
+                    result = browser.ShowDialog(win32);
+                }
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    //conferma la scelta
+                    return new[] { browser.SelectedPath };
+                }
+
+                return new string[0];
+            }
+        }
+
+        private class Win32Window
+            : System.Windows.Forms.IWin32Window
+        {
+            public IntPtr Handle { get; set; }
+        }
+#endif
+
+    }
+}
